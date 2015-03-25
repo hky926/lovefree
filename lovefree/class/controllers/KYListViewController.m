21ebab2c327b2c88cee1b9546480d470df885f80
hky926 <kaiyuanhuai@gmail.com>
2015-03-25 12:51:48 +0800

    "fix"

--- lovefree/class/controllers/KYListViewController.m
+++ lovefree/class/controllers/KYListViewController.m
@@ -17,9 +17,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    [self addi]
+    
 }
-
+-(void) createTitleView
+{
+    
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
